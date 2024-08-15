15c62493eabac0d7e90d8164237f4c6f8d186480
yu <3215214635@qq.com>
2024-08-15 20:59:55 +0800

    改用抽象工厂模式重新设计

--- src/main/java/com/yu/yucache/factory/YuCacheFactory.java
+++ src/main/java/com/yu/yucache/factory/YuCacheFactory.java
@@ -1,56 +1,83 @@
 package com.yu.yucache.factory;
 
+import com.yu.yucache.annotate.EnableYuCache;
 import com.yu.yucache.defaults.YuDefaultFirstCache;
 import com.yu.yucache.defaults.YuDefaultSecondCache;
 import com.yu.yucache.defaults.YuDefaultsFirstConfigProperties;
 import lombok.Data;
 import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.Map;
 
 
 @Data
 public class YuCacheFactory<T> {
 
 
-    private YuCacheFirstFactory yuCacheFirstFactory=new YuDefaultFirstCache();
+    @Resource
+    private ApplicationContext applicationContext;
 
-    public YuCacheSecondFactory yuCacheSecondFactory=new YuDefaultSecondCache();
+    private YuCacheFirstFactory yuCacheFirstFactory = new YuDefaultFirstCache();
 
-    private YuDefaultsFirstConfigProperties properties=new YuDefaultsFirstConfigProperties();
+    public YuCacheSecondFactory yuCacheSecondFactory = new YuDefaultSecondCache();
 
-    private T bean=null;
+    private YuDefaultsFirstConfigProperties properties = new YuDefaultsFirstConfigProperties();
 
-    public YuCacheFactory(){
+    private T bean = null;
+
+    public YuCacheFactory() {
 
     }
 
 
-    public YuCacheFactory(YuCacheFirstFactory yuCacheFirstFactory){
-        this.yuCacheFirstFactory=yuCacheFirstFactory;
-        this.properties=null;
+    /**
+     * HashMap<String, Boolean> map = new HashMap<>();
+     * log.info("同时加载配置");
+     * Object object = null;
+     * Map<String, Object> enAble = this.applicationContext.getBeansWithAnnotation(EnableYuCache.class);
+     * if (enAble.isEmpty()) {
+     * throw new RuntimeException("请在启动类添加启动注解EnableYuCache");
+     * }
+     * if (enAble.size() != 1) {
+     * throw new RuntimeException("EnableYuCache注解唯一");
+     * }
+     *
+     * @param yuCacheFirstFactory
+     */
+
+    public YuCacheFactory(YuCacheFirstFactory yuCacheFirstFactory) {
+        Map<String, Object> enAble = this.applicationContext.getBeansWithAnnotation(EnableYuCache.class);
+        if (enAble.isEmpty()) {
+            throw new RuntimeException("请在启动类添加启动注解EnableYuCache");
+        }
+        if (enAble.size() != 1) {
+            throw new RuntimeException("EnableYuCache注解唯一");
+        }
+
+
+
+        this.yuCacheFirstFactory = yuCacheFirstFactory;
+        this.properties = null;
     }
+
     public YuCacheFactory(YuCacheFirstFactory yuCacheFirstFactory,
                           YuCacheSecondFactory yuCacheSecondFactory,
-                          YuDefaultsFirstConfigProperties properties){
+                          YuDefaultsFirstConfigProperties properties) {
         this.yuCacheFirstFactory = new YuDefaultFirstCache(properties);
         this.yuCacheSecondFactory = yuCacheSecondFactory;
         this.properties = properties;
     }
 
-    public YuCacheFactory(YuDefaultsFirstConfigProperties properties){
+    public YuCacheFactory(YuDefaultsFirstConfigProperties properties) {
         this.yuCacheFirstFactory = new YuDefaultFirstCache(properties);
         this.properties = properties;
     }
 
 
-    public YuCacheFactory(YuCacheSecondFactory yuCacheSecondFactory){
+    public YuCacheFactory(YuCacheSecondFactory yuCacheSecondFactory) {
         this.yuCacheSecondFactory = yuCacheSecondFactory;
     }
-
-
-
 
 
 }
